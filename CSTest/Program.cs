52f2995c7dd4c544e405a6f2395497b8b2066233
Mateusz Kozlowski <mkozlow2@gmail.com>
2018-04-05 16:14:28 +0200

    Pierwszy program; tablica args

--- CSTest/Program.cs
+++ CSTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSTest
 {
@@ -10,6 +6,12 @@ namespace CSTest
     {
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine("Dzień dobry");
+
         }
     }
 }
