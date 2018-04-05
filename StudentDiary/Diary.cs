61c8fa233353e776343e7c353252b44b4429a20b
Mateusz Kozlowski <mkozlow2@gmail.com>
2018-04-05 20:37:12 +0200

    Wprowadzenie do klas; klasa dziennik

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        private List<float> ratings;
+
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, i = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            return sum / ratings.Count();
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(; ; )
+            {
+               // Console.Write("Podaj ocenę: ");
+               // Diary.AddRating(float.Parse(Console.ReadLine()));
+            }
+        }
+    }
+}
